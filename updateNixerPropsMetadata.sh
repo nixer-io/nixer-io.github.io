08eab1f2763dff778b10700546c8160bb524e126
Grzegorz Ćwiak <grzegorz.cwiak@crosswordcybersecurity.com>
2020-04-06 13:27:22 +0200

    chore: Change the updateNixerPropsMetadata script to use local artifacts instead of downloading from maven repo.

--- updateNixerPropsMetadata.sh
+++ updateNixerPropsMetadata.sh
@@ -5,18 +5,23 @@ set -o pipefail
 set -o nounset
 
 # Script for automatic updating spring-configuration-metadata.json files used for rendering Nixer properties documentatnion.
-# Fetches the metadata from Nixer artefacts deployed to Maven Centaral repository (repo1.maven.org mirror used explicitly for simplicity)
-# and copies it to the metadata files in this docs codebase, effectively updating them or leaving intact if no difference.
+# Uses results of nixer-spring-plugin project build execution, i.e. generated JARs, as sources for the update.
+# Copies the metadata from the source JARs to the metadata files in this documentation codebase,
+# effectively updating them or leaving intact if no difference.
 #
 # All changes are done locally and need to be commited.
 
-MVN_REPO_BASE_URL="https://repo1.maven.org/maven2/io/nixer"
 NIXER_PLUGIN_PREFIX="nixer-plugin"
 
-VERSION=${1:-}
+NIXER_CODEBASE_DIR=${1:-}
 
-if [[ -z ${VERSION} ]]; then
-  echo "Missing mandatory Nixer release version argument, e.g. 0.1.2.3"
+if [[ -z ${NIXER_CODEBASE_DIR} ]]; then
+  echo "Missing mandatory argument: nixer-spring-plugin codebase directory."
+  exit 1
+fi
+
+if [[ ! -d ${NIXER_CODEBASE_DIR} ]]; then
+  echo "nixer-spring-plugin codebase directory '${NIXER_CODEBASE_DIR}' does not exist."
   exit 1
 fi
 
@@ -26,24 +31,36 @@ if [ -n "$(git status --untracked-files=no --porcelain)" ]; then
 fi
 
 echo ""
-echo "==> Updating Nixer properties metadata to version ${VERSION}"
+echo "==> Updating Nixer properties metadata using nixer-spring-plugin codebase: ${NIXER_CODEBASE_DIR}"
 echo ""
 
 for module in "captcha" "core" "pwned-check" "stigma"
 do
   echo "---> Updating metadata for module: ${module}"
 
-  JAR_FILE_NAME=${NIXER_PLUGIN_PREFIX}-${module}-${VERSION}.jar
+  BUILD_DIR=${NIXER_CODEBASE_DIR}/${module}/build/libs
 
-  # example url: https://repo1.maven.org/maven2/io/nixer/nixer-plugin-stigma/0.1.1.2/nixer-plugin-stigma-0.1.1.2.jar
-  wget --quiet "${MVN_REPO_BASE_URL}/${NIXER_PLUGIN_PREFIX}-${module}/${VERSION}/${JAR_FILE_NAME}"
+  if [[ ! -d ${BUILD_DIR} ]]; then
+    echo "Build directory '${BUILD_DIR}' does not exist. Was build of nixer-spring-plugin executed?"
+    exit 1
+  fi
 
-  unzip -p "${JAR_FILE_NAME}" META-INF/spring-configuration-metadata.json > "_data/nixer-props-metadata/${module}/spring-configuration-metadata.json"
+  SOURCE_JAR_PATH=$(find "${NIXER_CODEBASE_DIR}/${module}/build/libs" -type f -name "${NIXER_PLUGIN_PREFIX}-${module}*.jar")
 
-  rm "${JAR_FILE_NAME}"
+  if [[ ! -f ${SOURCE_JAR_PATH} ]]; then
+    echo "Source JAR not found. Was build of nixer-spring-plugin executed?"
+    exit 1
+  fi
 
-  echo "---> ${module} metadata updated:"
-  echo "     _data/nixer-props-metadata/${module}/spring-configuration-metadata.json"
+  echo ""
+  echo "  -> Using source:  $(basename "${SOURCE_JAR_PATH}")"
+  echo ""
+
+  unzip -p "${SOURCE_JAR_PATH}" META-INF/spring-configuration-metadata.json > "_data/nixer-props-metadata/${module}/spring-configuration-metadata.json"
+
+  echo "  -> ${module} metadata updated:"
+  echo "       _data/nixer-props-metadata/${module}/spring-configuration-metadata.json"
+  echo ""
   echo ""
 
 done
